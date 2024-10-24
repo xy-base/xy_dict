946b2988d90f1b27f36a1ad2e9089e62bd2f0275
yuyanget <845262968@qq.com>
2024-10-24 20:27:34 +0800

    fix 修改时间

--- xy_dict/execute/Main.py
+++ xy_dict/execute/Main.py
@@ -7,7 +7,7 @@ __doc__ = 'Main'
   * @Author  :   余洋
   * @Version :   1.0
   * @Contact :   yuyangit.0515@qq.com
-  * @License :   (C)Copyright 2019-2023, Ship of Ocean
+  * @License :   (C)Copyright 2019-2024, Ship of Ocean
   * @Desc    :   None
 '''
 from xy_dict.Dict import Dict
